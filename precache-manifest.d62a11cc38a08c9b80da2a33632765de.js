f1aab5341df8edf639daf545f1a06bf8c40c78d4
Alex5678910 <sgs1986@yandex.ru>
2020-10-20 00:34:36 +0400

    Updates

---
+++ precache-manifest.d62a11cc38a08c9b80da2a33632765de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d2bc636c7372c59212952ddf793aac",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e17f21d8cefbec88e7d1",
+    "url": "/my-app/static/css/main.16009c09.chunk.css"
+  },
+  {
+    "revision": "ecc7a7dd4b5fc25f82e4",
+    "url": "/my-app/static/js/2.1128c97d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-app/static/js/2.1128c97d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e17f21d8cefbec88e7d1",
+    "url": "/my-app/static/js/main.6333b3cb.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/my-app/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/my-app/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "4100536eacd1ec2babd0f95d5cc80b6c",
+    "url": "/my-app/static/media/picture01.4100536e.png"
+  },
+  {
+    "revision": "f5e8d92aa4dd9dafe35e835a188839de",
+    "url": "/my-app/static/media/picture02.f5e8d92a.png"
+  },
+  {
+    "revision": "c9a1bda78cf513ef8919c661432faa2e",
+    "url": "/my-app/static/media/picture03.c9a1bda7.png"
+  },
+  {
+    "revision": "89516e6f15e39a2b7c882acf87d31a1b",
+    "url": "/my-app/static/media/picture04.89516e6f.png"
+  },
+  {
+    "revision": "65cd01f6cf0394341a069eb733176374",
+    "url": "/my-app/static/media/picture05.65cd01f6.png"
+  },
+  {
+    "revision": "4989a2d1b2079d02b4cba05641185f78",
+    "url": "/my-app/static/media/picture06.4989a2d1.png"
+  }
+]);
